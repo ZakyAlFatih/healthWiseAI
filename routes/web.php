b7af3c7b618dd311addaf4b865a7ec0d63f3e70c
ZakyAlFatih <111637188+ZakyAlFatih@users.noreply.github.com>
2024-11-21 10:57:36 +0700

    Ai chat feature

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use App\Http\Controllers\healthWiseController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/FoodRec', [healthWiseController::class, 'tampil'])->name('healthWise.tampil');
+
+
+Route::get('/chat', [ChatController::class, 'showChat']);
+
+// Handle the chat message (POST request)
+Route::post('/chat', [ChatController::class, 'getChatResponse']);
